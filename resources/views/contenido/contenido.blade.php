a01d5b67bbb6034e8be063f4005cc45213b9b96c
Ernesto <49647948+Ernestoale98@users.noreply.github.com>
2019-04-23 21:53:51 -0500

    Features/main menu (#2)
    * Copilando archivos css y javacript
    
    * Mix laravel fixed
    
    * implementation of vue.js
    
    * implementation of vue.js fixed
    
    * configuration of the menu with vue.js

--- resources/views/contenido/contenido.blade.php
+++ resources/views/contenido/contenido.blade.php
@@ -1,4 +1,43 @@
     @extends('principal')
-    @section('contenido')    
-    <example-component></example-component>
+    @section('contenido')
+    <template v-if="menu==0">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==1">
+        <h1>Variable menu es igual a 1 </h1>
+    </template>
+    <template v-if="menu==2">
+        <h1>Variable menu es igual a 2 </h1>
+    </template>
+    <template v-if="menu==3">
+        <h1>Variable menu es igual a 3 </h1>
+    </template>
+    <template v-if="menu==4">
+        <h1>Variable menu es igual a 4 </h1>
+    </template>
+    <template v-if="menu==5">
+        <h1>Variable menu es igual a 5 </h1>
+    </template>
+    <template v-if="menu==6">
+        <h1>Variable menu es igual a 6 </h1>
+    </template>
+    <template v-if="menu==7">
+        <h1>Variable menu es igual a 7 </h1>
+    </template>
+    <template v-if="menu==8">
+        <h1>Variable menu es igual a 8 </h1>
+    </template>
+    <template v-if="menu==9">
+        <h1>Variable menu es igual a 9 </h1>
+    </template>
+    <template v-if="menu==10">
+        <h1>Variable menu es igual a 10 </h1>
+    </template>
+    <template v-if="menu==11">
+        <h1>Variable menu es igual a 11 </h1>
+    </template>
+    <template v-if="menu==12">
+        <h1>Variable menu es igual a 12 </h1>
+    </template>
+
     @endsection
