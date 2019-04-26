1a0e0293ba3ce92267183f57ea9ae342ce12c406
Ernestoale98 <ernesto.munoz@cceo.com.mx>
2019-04-26 17:04:49 -0500

    login without logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,51 @@
 <?php
 
+/**
+ *
+ * @category  Login
+ * @version   1.0
+ * @since     2019-24-04
+ * @author    Ernesto <ernesto.munoz@cceo.com.mx>
+ */
+
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function showLoginForm()
     {
-        $this->middleware('guest')->except('logout');
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (Auth::attempt(['usuario' => $request->usuario, 'password' => $request->password, 'condicion' => 1])) {
+            return redirect()->route('main');
+        }
+
+        return back()
+            ->withErrors(['usuario' => trans('auth.failed')])
+            ->withInput(request(['usuario']));
+    }
+
+    protected function validateLogin(Request $request)
+    {
+        $this->validate($request, [
+            'usuario' => 'required|string',
+            'password' => 'required|string'
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        return redirect('/');
     }
 }
