eb86c1cd7e3b30babb3211f89d7600ef72a1af5e
Ernesto <49647948+Ernestoale98@users.noreply.github.com>
2019-04-28 19:28:05 -0500

    Features/login (#9)
    * login without logout
    
    * login done

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -9,8 +9,8 @@
     <meta name="author" content="Incanatoit.com">
     <meta name="keyword" content="Sistema ventas Laravel Vue Js, Sistema compras Laravel Vue Js">
     <link rel="shortcut icon" href="img/favicon.png">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Sistema Ventas - IncanatoIT</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Icons -->
     <link href="css/plantilla.css" rel="stylesheet">
 </head>
@@ -54,26 +54,34 @@
                     </div>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link  nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
                         <img src="img/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-                        <span class="d-md-down-none">admin </span>
+                        <span class="d-md-down-none">{{Auth::user()->usuario}} </span>
                     </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <div class="dropdown-header text-center">
-                            <strong>Cuenta</strong>
-                        </div>
-                        <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Perfil</a>
-                        <a class="dropdown-item" href="#"><i class="fa fa-lock"></i> Cerrar sesión</a>
-                    </div>
+                </li>
+                <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fa fa-lock"></i> Cerrar sesión</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        {{ csrf_field() }}
+                    </form>
                 </li>
             </ul>
         </header>
 
         <div class="app-body">
 
-            <!-- /Para incluir una vista blade en otra se utiliza el comando include -->
-            @include('plantilla.sidebar')
-            <!-- /Inicio del contenido principal -->
+            @if(Auth::check())
+                 @if (Auth::user()->idrol == 1)
+                     @include('plantilla.sidebaradministrador')
+                @elseif (Auth::user()->idrol == 2)
+                    @include('plantilla.sidebarvendedor')
+                @elseif (Auth::user()->idrol == 3)
+                    @include('plantilla.sidebaralmacenero')
+                @else
+                 @endif
+            @endif
+            <!-- Contenido Principal -->
             @yield('contenido')
             <!-- /Fin del contenido principal -->
         </div>
@@ -82,6 +90,7 @@
         <span><a href="http://www.incanatoit.com/">IncanatoIT</a> &copy; 2017</span>
         <span class="ml-auto">Desarrollado por <a href="http://www.incanatoit.com/">IncanatoIT</a></span>
     </footer>
+
 
     <script src="js/app.js"></script>
     <script src="js/plantilla.js"></script>
